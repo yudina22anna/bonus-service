3186b8718c66e95edf47e1bf68a4f544100b6e18
annayudina <yudina.anna.zi15@gmail.com>
2022-08-16 19:07:02 +0300

    fixed #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         BonusService service = new BonusService();
-    long bonus;
         long amount = 1000_60;
         boolean registered = true;
 
-        service.calculate(amount, registered);
+        long bonus = service.calculate(amount, registered);
+        System.out.println(bonus);
+
     }
 }
 
